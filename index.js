c37ed249008bd35fac6f4491b784a8c291d6b945
Ujwal164 <ujwalbagul9@gmail.com>
2026-09-24 16:50:55 +0530

    added delete task

--- index.js
+++ index.js
@@ -55,3 +55,18 @@ app.post("/edit", async(req, res) => {
   console.log(err);
 }
 });
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId;
+  try{
+    await db.query(`
+      DELETE FROM items
+      WHERE id =$1`,[id]);
+    res.redirect("/");
+  } catch(err){
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
