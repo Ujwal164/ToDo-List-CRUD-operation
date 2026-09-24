935840c2e5995ef86508022af46eb19188b0368c
Ujwal164 <ujwalbagul9@gmail.com>
2026-09-24 16:48:54 +0530

    initiate get method

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "ujwal@164",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [];
+
+app.get("/", async (req, res) => {
+ try {
+    const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+    items = result.rows;
+
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
